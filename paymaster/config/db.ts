2389a46691656201241d69047e05c2fae0895e61
Max Lozovoy <max.lozovoy@devico.io>
2024-09-26 14:37:53 +0300

    chore: update default db settings

--- paymaster/config/db.ts
+++ paymaster/config/db.ts
@@ -3,7 +3,7 @@ export const DB_DATABASE = process.env.DB_DATABASE || "gas-tank";
 export const DB_HOST = process.env.DB_HOST || "host.docker.internal";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 export const DB_PORT = Number(process.env.DB_PORT) || 5432;
-export const DB_USERNAME = process.env.DB_USERNAME || "murzik";
+export const DB_USERNAME = process.env.DB_USERNAME || "postgres";
 export const DB_SSL_MODE = process.env.DB_SSL_MODE
   ? (process.env.DB_SSL_MODE || "").toLowerCase() === "true"
   : NODE_ENV !== "local";
